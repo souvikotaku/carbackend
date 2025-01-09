0db67180ea598b45dbf02b9cbc7e830e95dfd14f
souvikotaku <manson1989@>
2025-01-09 15:21:44 +0530

    changes

--- index.js
+++ index.js
@@ -2,25 +2,33 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import cors from "cors"; // Use import for CORS
 import route from "./routes/userRoute.js";
 
 const app = express();
 
-app.use(bodyParser.json());
 dotenv.config();
-const PORT = 8000 || 5000;
-const MONGOURL = "mongodb+srv://souvikpunk:manson1@cluster0.h88lc.mongodb.net/";
 
+// Middleware
+app.use(cors()); // Enable CORS
+app.use(bodyParser.json());
+
+// Environment Variables
+const PORT = process.env.PORT || 5000; // Use environment variable or default to 8000
+const MONGOURL = process.env.MONGO_URL;
+
+// Connect to MongoDB and Start Server
 mongoose
-  .connect(MONGOURL)
+  .connect(MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true }) // Pass connection options to avoid warnings
   .then(() => {
-    console.log("database connected successfully");
+    console.log("Database connected successfully");
     app.listen(PORT, () => {
-      console.log(`server is running on port ${PORT}`);
+      console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
-    console.log(err);
+    console.error("Error connecting to database:", err);
   });
 
+// Routes
 app.use("/api/user", route);
